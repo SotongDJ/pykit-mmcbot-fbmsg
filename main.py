1c44047a247eff186402215c83cff439bdd2a0e6
David Soh <sotong0221@gmail.com>
2017-05-10 20:13:01 +0800

    fix

--- main.py
+++ main.py
@@ -15,16 +15,12 @@ def reply(user_id, msg):
     resp = requests.post("https://graph.facebook.com/v2.6/me/messages?access_token=" + ACCESS_TOKEN, json=data)
     print(resp.content)
 
-@app.route('/', methods=['GET'])
-def handle_verification():
-    return request.args['hub.challenge']
-
 @app.route('/', methods=['POST'])
 def handle_incoming_messages():
     data = request.json
     sender = data['entry'][0]['messaging'][0]['sender']['id']
     message = data['entry'][0]['messaging'][0]['message']['text']
-    reply(sender, message[::-1])
+    reply(sender, message+message)
 
     return "ok"
 
